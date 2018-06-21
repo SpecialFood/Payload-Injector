c5b0f516bd912974b2dcb9614a4aec875da1933a
SpecialFood <SpecialFood456@gmail.com>
2018-06-21 12:10:59 +0300

    Payload Injector

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Payload Injector
+Payload Injector is a tool I've made for the PS4 Jailbreak, based on version 5.05.
+
+based on Specter(Cryptogenic)'s PS4-5.05-Kernel-Exploit
+
+The tool would probably not work on 4.55 so I wouldn't suggest trying.
+
+In order to use this tool the user should first convert his payload to homebrew.js using the included tool bin2homebrew (watch at the bottom).
+
+After converting simply drag the homebrew file into the Payload Injector folder.
+
+This folder can now be sent as an exploit, the user can use Al-Azif's ps4-exploit-host for example in order to send it.
+
+# bin2homebrew :
+bin2homebrew is a tool I've made in order to convert payloads to homebrew files, so that the Payload Injector can use those converted payloads.
+
+based on iCyb3r's bin2html
+
+Drag *payload*.bin to the bin2homebrew.exe file.
+
+Now the window shall say that it's converting it to a homebrew.js file.
+
+Upon completion a homebrew.js file will be generated in the same folder the payload is in, and the user may press any key to close the window.
---
+++ tool/Source Code/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bin2homebrew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            Console.Write("Process begins.\n");
+
+            foreach (string arg in args)
+            {
+                byte[] argData = File.ReadAllBytes(arg);
+                string
+                    homebrew = "function writeHomebrewEN(p, addr) {\n"
+                    , argText = BitConverter.ToString(argData).Replace("-", "");
+
+                int
+                    counter = 0
+                    , argBytes = argText.Length / 8
+                    , argRemainingBits = argText.Length % 8;
+
+                List<string> argDataTextList =
+                        Enumerable.Range(0, argBytes)
+                        .Select(i => BitConverter.ToString(StringToByteArrayReversed(argText.Substring(8 * i, 8))).Replace("-", "").ToLowerInvariant()).ToList();
+
+                Console.Write("Converting {0} to homebrew.js.\n", Path.GetFileName(arg));
+
+                //if (Convert.ToBoolean(argRemainingBits))
+                    //argDataTextList.Add(int.Parse(BitConverter.ToString(StringToByteArrayReversed(argText.Substring(argText.Length - argRemainingBits))).Replace("-", ""), System.Globalization.NumberStyles.HexNumber).ToString("X8"));
+
+                foreach (string argDataTextListCell in argDataTextList)
+                    homebrew += "\tp.write4(addr.add32(0x" + (counter++ * 4).ToString("X8").ToLowerInvariant() + "), 0x" + argDataTextListCell + ");\n";
+
+                homebrew += "}";
+
+                string homebrewFile =
+                    Path.GetDirectoryName(arg)
+                    + Path.DirectorySeparatorChar
+                    + "homebrew.js";
+
+                File.WriteAllText(homebrewFile, homebrew);
+
+                Console.Write("Successfully converted.\n");
+            }
+
+            Console.Write("Process completed.\nPress any key to close this window...");
+
+            Console.ReadKey();
+        }
+        public static byte[] StringToByteArrayReversed(string hex)
+        {
+            byte[] hexRev =
+                Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+
+            Array.Reverse(hexRev);
+            return hexRev;
+        }
+    }
+}
